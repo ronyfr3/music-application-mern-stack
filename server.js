a1c639fd2d76bea2d5a6139bc23fdf4ce05b566f
ronyfr3 <babamarony@gmail.com>
2021-04-06 14:14:27 +0600

    hey

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 //   });
 // }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running on port", PORT);
 });
